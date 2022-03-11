736934f18276d7b42e281f4739863aa5a64defdd
MagicSkySword <skyswordkill@qq.com>
2022-03-11 18:21:41 +0800

    firstUpdate

---
+++ NextExamplePlugin/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BepInEx;
+using SkySwordKill.Next;
+
+namespace SkySwordKill.NextCommandExamplePlugin
+{
+    [BepInPlugin("skyswordkill.plugin.NextCommandExamplePlugin", "NextCommandExamplePlugin", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // 注册 CostMoney 事件
+            DialogAnalysis.RegisterCommand("CostMoney",new CostMoneyEvent());
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Next插件范例
+
+Next 地址：https://github.com/magicskysword/Next
+
+* NextExampleCommandPlugin
+
+  为Next添加新的指令的范例插件
