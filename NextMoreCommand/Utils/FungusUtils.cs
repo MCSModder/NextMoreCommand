3361bffadf95ee5fccc790c9b1a74bf08c639625
深淵の鴿子 <71170059+ZerxZ@users.noreply.github.com>
2022-11-18 18:38:39 +0100

    Update FungusUtils.cs

--- NextMoreCommand/Utils/FungusUtils.cs
+++ NextMoreCommand/Utils/FungusUtils.cs
@@ -6,14 +6,14 @@ using Newtonsoft.Json;
 using SkySwordKill.Next;
 using SkySwordKill.Next.FCanvas;
 using UnityEngine;
-
+using Object =UnityEngine.Object;
 namespace SkySwordKill.NextMoreCommand.Utils
 {
     public static class FungusUtils
     {
-        private static readonly Dictionary<string, Flowchart> _flowcharts = new Dictionary<string, Flowchart>();
+        private static readonly Dictionary<string, GameObject> _flowcharts = new Dictionary<string, Flowchart>();
 
-        public static Dictionary<string, Flowchart> Flowcharts
+        public static Dictionary<string, GameObject> Flowcharts
         {
             get
             {
@@ -31,11 +31,10 @@ namespace SkySwordKill.NextMoreCommand.Utils
             if (_flowcharts.Count != 0) return;
             
             var go = Resources.LoadAll<GameObject>("");
-            var select = go
-                .Select(g => g.GetComponentInChildren<Flowchart>());
             List<string> list = new List<string>();
-            foreach (var flowchart in select)
+            foreach (var gameObject in go)
             {
+                var flowchart =gameObject.GetComponentInChildren<Flowchart>();
                 if (flowchart == null)
                     continue;
 
@@ -48,7 +47,7 @@ namespace SkySwordKill.NextMoreCommand.Utils
                     }
                     else
                     {
-                        _flowcharts.Add(name, flowchart);
+                        _flowcharts.Add(name, gameObject);
                         Main.LogInfo($"添加{name} 到 FungusUtils.Flowcharts");
                     }
                   
@@ -69,10 +68,14 @@ namespace SkySwordKill.NextMoreCommand.Utils
 
         public static bool TryGetFlowchart(string key, out Flowchart flowchart)
         {
-            if (!Flowcharts.TryGetValue(key, out flowchart))
+            if (!Flowcharts.TryGetValue(key, out GameObject gameObject))
             {
+                flowchart = null;
                 return false;
             }
+
+            flowchart =Object.Instantiate(gameObject).GetComponentInChildren<Flowchart>();
+            flowchart.StopBlock()
 
             return true;
         }
