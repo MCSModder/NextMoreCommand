26c732700152f6fd9616700f8bf712dc5c3813c6
深渊の鸽子 <ghzero97@gmail.com>
2023-05-02 01:20:42 +0100

    更新

--- BetterSeidOptimization/BetterSeidOptimization.cs
+++ BetterSeidOptimization/BetterSeidOptimization.cs
@@ -1,5 +1,7 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using SkySwordKill.Next.Mod;
 using UniqueCream.CustomDungeons.NextSupport.NextEvent.DungeonEvent;
 
 namespace Zerxz.BetterSeidOptimization;
@@ -10,12 +12,37 @@ public class BetterSeidOptimization : BaseUnityPlugin
 {
     private Harmony _harmony;
     public static BetterSeidOptimization Inst;
+    public ConfigEntry<bool> BetterMode;
     private void Awake()
     {
+        BetterMode = Config.Bind("开启优化模式", "Seid优化", false, "");
         Inst = this;
         _harmony = new Harmony("zerxz.plugin.BetterSeidOptimization");
         _harmony.PatchAll();
+        ModManager.ModLoadComplete += ModLoadComplete;
+        ModManager.ModSettingChanged += ModSettingChanged;
 
+    }
+    private void InitSettingAction()
+    {
+        BetterMode.SettingChanged += (sender, args) =>
+        {
+            ModManager.SetModSetting("NMC_BetterSeidMode", BetterMode.Value);
+        };
+
+    }
+    private void ModSettingChanged()
+    {
+        if (ModManager.TryGetModSetting("NMC_BetterSeidMode", out bool betterSeid))
+        {
+            BetterMode.Value = betterSeid;
+        }
+
+    }
+    private void ModLoadComplete()
+    {
+        ModManager.SetModSetting("NMC_BetterSeidMode",  BetterMode.Value);
+        
     }
     private void OnDestroy()
     {
